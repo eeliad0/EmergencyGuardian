e8b015f0cc3afd5c2bef4b3f7b579aed1fe7c140
eeliad0 <eeliad02@ucy.ac.cy>
2023-11-07 21:13:33 +0200

    Added login and splashscreen xml & code

---
+++ app/src/main/java/com/example/emergencyguardian/SelfDefense.java
@@ -0,0 +1,6 @@
+package com.example.emergencyguardian;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SelfDefense extends AppCompatActivity {
+}
--- app/src/main/java/com/example/emergencyguardian/SignupScreen.java
+++ app/src/main/java/com/example/emergencyguardian/SignupScreen.java
@@ -1,6 +1,27 @@
 package com.example.emergencyguardian;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class SignupScreen extends AppCompatActivity {
-}
+public class SignupScreen extends AppCompatActivity implements View.OnClickListener {
+
+        TextView signIn;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_signup_screen);
+            signIn = findViewById(R.id.sign_in);
+            signIn.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if(view.getId()==R.id.sign_in){
+                finish();
+            }
+        }
+    }
+
