3206f6d7e9f222126676d1cc1e6c8aa5810ea1b8
Antria Argyrou <antria.argyrou2001@gmail.com>
2023-11-19 18:36:17 +0200

    Siren play fragment

--- app/src/main/java/com/example/emergencyguardian/SirenPlay.java
+++ app/src/main/java/com/example/emergencyguardian/SirenPlay.java
@@ -1,5 +1,39 @@
 package com.example.emergencyguardian;
 
+import android.graphics.drawable.Drawable;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.drawable.DrawableCompat;
 import androidx.fragment.app.Fragment;
 public class SirenPlay extends Fragment {
+    private MediaPlayer mediaPlayer;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_siren_play, container, false);
+
+        Button button = view.findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+                    mediaPlayer.stop();
+                    mediaPlayer.release();
+                    mediaPlayer = null;
+                } else {
+                    mediaPlayer = MediaPlayer.create(getActivity(), R.raw.siren);
+                    mediaPlayer.start();
+                }
+            }
+        });
+
+        return view;
+
+    }
 }
